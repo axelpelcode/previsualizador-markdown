758055975281a81f1adbd835cc04e04a8d6eba66
axelpecode <axelpelassini@hotmail.com>
2023-05-19 10:49:15 -0300

    commitComponents

---
+++ src/componentes/editor.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Editor() {
+    return (
+    <div>
+        <label>
+            Editor
+            <textarea id="editor" />
+        </label>
+    </div>
+    )
+};
+
+export default Editor;
---
+++ src/componentes/preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Editor from './editor';
+
+function Preview() {
+    return (
+    <div>
+        <div id="preview"></div>
+    </div>
+    )
+};
+
+export default Preview;
