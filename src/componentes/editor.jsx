77e569eac3802520f290b5a7221f2e1aef7eda26
axelpecode <axelpelassini@hotmail.com>
2023-05-19 11:39:56 -0300

    commit02

--- src/componentes/editor.jsx
+++ src/componentes/editor.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Editor() {
+    const [textInit, setText] = useState("texto inicial");
+
+    const TextDefault = () => {
+        <h1>Texto Default</h1>
+    };
+
+    const TextEdit = () => {
+        <h1>Texto nuevo</h1>
+    };
+
     return (
     <div>
         <label>
             Editor
-            <textarea id="editor" />
+            <textarea 
+                id="editor"
+                defaultValue={TextDefault}
+                rows={4}
+                cols={30}
+                 />
         </label>
     </div>
     )
