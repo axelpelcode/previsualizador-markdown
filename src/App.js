e49f5f52b37440ec21a1f9aa8aa8e4fb427f6cb4
axelpecode <axelpelassini@hotmail.com>
2023-05-22 09:34:25 -0300

    commit03

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import './App.css';
 
 function App() {
 
   const [input, setInput] = useState();
+
+  const Component = ({children}) => {
+    return (
+      <SyntaxHighlighter language='javascript' style={docco}>
+        {children}
+      </SyntaxHighlighter>
+    );
+  };
 
   return (
    <div className="App">
@@ -18,9 +28,15 @@ function App() {
                 autoFocus
                  />
         </label>
-        <ReactMarkdown className="markdown" >{input}</ReactMarkdown>
+        <ReactMarkdown 
+        className="markdown"
+        children={input}
+        components={{code: Component,}} 
+        >{input}</ReactMarkdown>
    </div>
   );
-}
+};
+
+
 
 export default App;
