bce86256f2d097f6ca5ee114a2284cbc7df7fe5a
axelpecode <axelpelassini@hotmail.com>
2023-05-27 07:58:11 -0300

    commit04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import './App.css';
 
 function App() {
 
-  const textDefault = "# Markdown previewer \r## -Code repository: \r### [Github](https://github.com/axelpelcode/previsualizador-markdown)"
+  const textDefault = "# Markdown previewer \n## -Examples: \n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n\tif (firstLine == '```' && lastLine == '```') {\n\t\treturn multiLineCode;\n\t}\n};\n``` \nhttps://github.com/axelpelcode/previsualizador-markdown or [Github](https://github.com/axelpelcode/previsualizador-markdown)";
 
   const [input, setInput] = useState(textDefault);
 
@@ -22,6 +23,7 @@ function App() {
   return (
    <div className="App">
         <label className="label">
+          <h2>Editor</h2>
             <textarea 
                 id="editor"
                 className="textarea"
@@ -30,17 +32,20 @@ function App() {
                 autoFocus
                  />
         </label>
-      <div id="preview">
+      <div id="preview" >
+        <h2>Preview</h2>
         <ReactMarkdown 
-        className="markdown"
-        children={input}
-        components={{code: Component,}} 
+        className= "markdown"
+        children= {input}
+        components= {{ code: Component, }}
+        remarkPlugins= {[  remarkGfm  ]} 
+        
         >{input}</ReactMarkdown>
       </div>
    </div>
   );
 };
 
-
+// dangerouwlySetInnerHTML={{ _html: marked(input, { Component: Component }) }}
 
 export default App;
